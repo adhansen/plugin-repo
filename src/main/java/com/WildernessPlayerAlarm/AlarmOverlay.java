7a643acabfd7b91525df559fe25fa5c83b8cbf03
Alexander Hansen <adhansen@umich.edu>
2024-10-28 17:39:54 -0700

    (#41) Sane alarm overlay implementation ala YvesW

--- src/main/java/com/WildernessPlayerAlarm/AlarmOverlay.java
+++ src/main/java/com/WildernessPlayerAlarm/AlarmOverlay.java
@@ -5,7 +5,6 @@ import javax.inject.Inject;
 import net.runelite.api.Client;
 import net.runelite.client.ui.overlay.OverlayLayer;
 import net.runelite.client.ui.overlay.OverlayPanel;
-import net.runelite.client.ui.overlay.components.LineComponent;
 
 public class AlarmOverlay extends OverlayPanel
 {
@@ -19,39 +18,33 @@ public class AlarmOverlay extends OverlayPanel
         this.config = config;
         this.client = client;
         setLayer(OverlayLayer.ABOVE_SCENE);
-        for(int i = 0; i < 100; ++i)
-        {
-            panelComponent.getChildren().add((LineComponent.builder())
-                    .left(" ")
-                    .build());
-        }
     }
 
     @Override
     public Dimension render(Graphics2D graphics)
     {
         FlashControl configuredSpeed = config.flashControl();
-        panelComponent.setPreferredSize(new Dimension(client.getCanvasWidth(), client.getCanvasHeight()));
 
         switch (configuredSpeed)
         {
             case OFF:
-                panelComponent.setBackgroundColor(transparent);
+                graphics.setColor(transparent);
                 break;
             case SOLID:
-                panelComponent.setBackgroundColor(config.flashColor());
+                graphics.setColor(config.flashColor());
                 break;
             default:
                 if ((client.getGameCycle() % config.flashControl().getRate()) >= (config.flashControl().getRate() / 2))
                 {
-                    panelComponent.setBackgroundColor(config.flashColor());
+                    graphics.setColor(config.flashColor());
                 } else
                 {
-                    panelComponent.setBackgroundColor(transparent);
+                    graphics.setColor(transparent);
                 }
                 break;
         }
-
-        return panelComponent.render(graphics);
+        //Fill the rectangle using the client width and height
+        graphics.fillRect(0, 0, client.getCanvasWidth(), client.getCanvasHeight());
+        return null;
     }
 }
